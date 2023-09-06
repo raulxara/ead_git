0467a0e0c820967b5396cf85746910164d465f6d
Raul Nascimento Cruz <raul.nascc98@gmail.com>
2023-09-05 21:24:50 -0300

    protecao de dados

--- pages/404.php
+++ pages/404.php
@@ -1,11 +1,8 @@
-<div style="position: absolute; left: 50%; top: 50%; transform: translate(-50%,-50%); -ms-transform: translate(-50%,-50%); text-align: center;" class="error-404">
-	<div class="container">
+<section class="error-404">
+	<div class="center">
 			<h1>ERROR 404</h1>
-			<h2><i class="fa fa-times"></i> Ops, essa página não pode ser acessada!</h2>
-			<p>Deseja voltar para a página de login? <a class="nav-link" href="<?php echo INCLUDE_PATH; ?>">Clique aqui!</a></p>
-		
+			<h2><i style="padding: 0 10px;" class="fa fa-times"></i>Ops, essa página não pode ser acessada!</h2>
+			<p>Deseja voltar para a página de login? <a href="<?php echo INCLUDE_PATH; ?>">Clique aqui!</a></p>
+	<div class="clear"></div>	
 	</div>
-</div> 
-
-	
-	
+</section>
